8f3a913d6cbcb9080723f7367d3c4aa72953d208
calvinmcelvain <133781550+calvinmcelvain@users.noreply.github.com>
2024-06-10 11:18:04 -0500

    Update: Stage 1 decision py format and popups

--- STG1_2/__init__.py
+++ STG1_2/__init__.py
@@ -1,5 +1,5 @@
 from otree.api import *
-
+from settings import DECISION_TIME, FEEDBACK_TIME
 
 doc = """
 Stage 1 Game
@@ -9,96 +9,69 @@ Stage 1 Game
 class C(BaseConstants):
     NAME_IN_URL = 'STG1_2'
     PLAYERS_PER_GROUP = 2
-    NUM_ROUNDS = 2
+    NUM_ROUNDS = 5
 
     # Payoff Dictionaries
     p1_payoff = {
-        1: {1: 200, 2: 0},
-        2: {1: 300, 2: 100}
+        'A': {'C': 200, 'D': 0},
+        'B': {'C': 300, 'D': 100}
     }
     p2_payoff = {
-        1: {1: 200, 2: 300},
-        2: {1: 0, 2: 100}
+        'A': {'C': 200, 'D': 300},
+        'B': {'C': 0, 'D': 100}
     }
 
     # Roles
-    player1_ROLE = 'Player 1'
-    player2_ROLE = 'Player 2'
+    p1_ROLE = 'Player 1'
+    p2_ROLE = 'Player 2'
+
 
 class Subsession(BaseSubsession):
-    # Grouping randomly
-    def creating_session(self):
-        self.group_randomly()
+    pass
+
 
 class Group(BaseGroup):
     pass
 
+
 class Player(BasePlayer):
-    # Setting choice field
+    # Form fields
     decision = models.StringField()
 
-    # Instructions Quiz
-    completedquiz = models.BooleanField(initial=False)
-
     # History
-    @property
     def other(self):
         return self.get_others_in_group()[0]
+    
 
     def total_payoff(self):
         total_payoff = sum([p.payoff for p in self.in_all_rounds()])
         return total_payoff
-    def Stage(self):
-        return 1
+    
+
+# Functions
+def creating_session(subsession):
+    subsession.group_randomly(fixed_id_in_group=True)
+    
 
 # PAGES
-class Instructions(Page):
-    def is_displayed(player: Player):
-        return player.round_number == 1
-
-class InstructionsQuiz(Page):
-    def is_displayed(player: Player):
-        return player.round_number == 1
-
-    @staticmethod
-    def live_method(player: Player, data):
-        correct_answers = [True, True, 0, 300]
-        player_answers = data['Q1', 'Q2', 'Q3', 'Q4']
-        if correct_answers == player_answers:
-            return player.completedquiz == True
-
-class Decision_Players(Page):
+class Decision_1(Page):
     form_model = 'player'
     form_fields = ['decision']
 
-class ResultsWaitPage(WaitPage):
+
+class PayoffWaitPage(WaitPage):
     # Calculating round payoffs
     def after_all_players_arrive(group: Group):
         player1 = group.get_player_by_role(C.Player1_ROLE)
         player2 = group.get_player_by_role(C.Player2_ROLE)
-        if player1.decision == 'A':
-            if player2.decision == 'C':
-                player1.payoff = C.payoff_AC
-                player2.payoff = C.payoff_AC
-            else:
-                player1.payoff = C.payoff_low
-                player2.payoff = C.payoff_high
-        else:
-            if player2.decision == 'C':
-                player1.payoff = C.payoff_high
-                player2.payoff = C.payoff_low
-            else:
-                player1.payoff = C.payoff_BD
-                player2.payoff = C.payoff_BD
+        player1.payoff = C.p1_payoff[player1.decision][player2.decision]
+        player2.payoff = C.p2_payoff[player1.decision][player2.decision]
 
-class PlayerResults(Page):
+
+class Results_2(Page):
     @staticmethod
     def vars_for_template(player: Player):
         return dict(history=player.in_all_rounds())
 
-class Stage1End(Page):
-    def is_displayed(player: Player):
-        return Player.round_number == C.NUM_ROUNDS
 
-
-page_sequence = [Instructions, InstructionsQuiz, Decision_Players, ResultsWaitPage, PlayerResults, Stage1End]
+page_sequence = [Decision_1, PayoffWaitPage, Results_2]
--- _static/general.js
+++ _static/general.js
@@ -72,3 +72,79 @@
         errorSpan.style.display = 'inline';
     }
   }
+
+
+// Decision button highlighting for player 1
+  function highlightRow(element) {
+    // Remove highlight from all cells
+    var cells = document.querySelectorAll('td.highlight-g');
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].classList.remove('highlight-g');
+    }
+
+    // Highlight the corresponding row cells to the right
+    var row = element.parentNode.parentNode;
+    var cellsToHighlight = row.querySelectorAll('td:nth-child(n+' + (element.parentNode.cellIndex + 2) + ')');
+    for (var j = 0; j < cellsToHighlight.length; j++) {
+        cellsToHighlight[j].classList.add('highlight-g');
+    }
+
+    // Change the button color to red
+    var confirmButton = document.getElementById('confirmButton');
+    confirmButton.style.backgroundColor = 'red';
+  }
+
+
+// Decision button highlighting for player 2
+  function highlightCol(element) {
+    // Remove highlight from all cells
+    var cells = document.querySelectorAll('td.highlight-g');
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].classList.remove('highlight-g');
+    }
+
+    // Highlight the rows below the clicked radio button in the same column
+    var cell = element.parentNode;
+    var column = cell.cellIndex;
+    var table = cell.closest('table');
+    var rows = table.querySelectorAll('tr');
+    for (var j = cell.parentNode.rowIndex + 1; j < rows.length; j++) {
+        var cellsToHighlight = rows[j].querySelectorAll('td:nth-child(' + (column + 1) + ')');
+        for (var k = 0; k < cellsToHighlight.length; k++) {
+            cellsToHighlight[k].classList.add('highlight-g');
+        }
+    }
+
+    // Change the button color to red
+    var confirmButton = document.getElementById('confirmButton');
+    confirmButton.style.backgroundColor = 'red';
+  }
+
+
+// Decision confirmation pop-up
+/// Decision Button
+  function confirmDecision(event) {
+    event.preventDefault(); // Prevents the form from submitting by default
+    const decision = document.getElementById("decision");
+
+    const decisionChecked = document.querySelector('input[name="pa_low_advice"]:checked');
+
+    if (!decisionChecked) {
+        lowError.textContent = "Field must be selected";
+        lowError.style.display = "block";
+    } else {
+        lowError.style.display = "none";
+    }
+
+    // Check if all three are checked before submitting the form
+    if (decisionChecked) {
+        var confirmationModal = new bootstrap.Modal(document.getElementById('confirmationModal'));
+        confirmationModal.show();
+
+        // Handle confirm button click inside the modal
+        document.getElementById("confirmButton").onclick = function () {
+            confirmationModal.hide();
+            document.forms[0].submit();
+        };
+    }
+  }
